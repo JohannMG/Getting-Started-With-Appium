38e64117af7f3693cf3685cf3df3d33d96c4bf3b
Johann Garces <JGarces@national.aaa.com>
2016-02-11 10:50:25 -0500

    Initial Commit

---
+++ appiumtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appiumtest
+//
+//  Created by Garces, Johann (TEKsystems) on 2/11/16.
+//  Copyright © 2016 aaa.johannmg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
